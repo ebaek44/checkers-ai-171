c7601e2fc17c5704031e21537c7f5d507a3fcb17
jordanazzam <jelazzam@uci.edu>
2025-11-10 04:11:31 +0000

    editing for simplicity

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -2,7 +2,7 @@ from BoardClasses import Move, Board
 from random import randint
 import copy
 
-def _flatten(buckets):
+def all_choices(buckets):
     out = []
     for bucket in buckets:
         out.extend(bucket)
@@ -19,7 +19,7 @@ class StudentAI:
         self.opponent = {1: 2, 2: 1}
 
     def get_move(self, move: Move):
-        # Apply opponent's move
+
         if move and getattr(move, "seq", None):
             if self.color is None:
                 self.color = 2
@@ -27,38 +27,32 @@ class StudentAI:
         else:
             if self.color is None:
                 self.color = 1
-        
-        #list of all legal moves
-        legal = _flatten(self.board.get_all_possible_moves(self.color))
+
+        legal = all_choices(self.board.get_all_possible_moves(self.color))
         if not legal:
             return Move([])
-        
-        # Evaluate each move and pick best
         best_move = legal[0]
         best_score = -999999
         
         for m in legal:
-            # Try the move
             test_board = copy.deepcopy(self.board)
             test_board.make_move(m, self.color)
-            
-            # Evaluate position
+
             score = self.evaluate(test_board)
             
             if score > best_score:
                 best_score = score
                 best_move = m
-        
-        # applies the best move and returns the best move
+
         self.board.make_move(best_move, self.color)
         return best_move
     
     def evaluate(self, board):
 
-        if self.color == 1:  # Black
+        if self.color == 1:
             my_pieces = board.black_count
             opp_pieces = board.white_count
-        else:  # White
+        else:
             my_pieces = board.white_count
             opp_pieces = board.black_count
         
