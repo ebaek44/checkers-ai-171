cf577d4684d7ae515fd14ac096743eaf8791a4f9
jordanazzam <jelazzam@uci.edu>
2025-11-10 01:59:51 +0000

    edits

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -1,27 +1,66 @@
-from random import randint
-from BoardClasses import Move
-from BoardClasses import Board
-#The following part should be completed by students.
-#Students can modify anything except the class name and exisiting functions and varibles.
-class StudentAI():
 
-    def __init__(self,col,row,p):
+from BoardClasses import Move, Board
+from random import randint
+import copy
+
+def _flatten(buckets):
+    out = []
+    for bucket in buckets:
+        out.extend(bucket)
+    return out
+
+class StudentAI:
+    def __init__(self, col, row, p):
         self.col = col
         self.row = row
         self.p = p
-        self.board = Board(col,row,p)
+        self.board = Board(col, row, p)
         self.board.initialize_game()
-        self.color = ''
-        self.opponent = {1:2,2:1}
-        self.color = 2
-    def get_move(self,move):
-        if len(move) != 0:
-            self.board.make_move(move,self.opponent[self.color])
+        self.color = None
+        self.opponent = {1: 2, 2: 1}
+
+    def get_move(self, move: Move):
+        # Apply opponent's move
+        if move and getattr(move, "seq", None):
+            if self.color is None:
+                self.color = 2
+            self.board.make_move(move, self.opponent[self.color])
         else:
-            self.color = 1
-        moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
-        return move
+            if self.color is None:
+                self.color = 1
+        
+        #list of all legal moves
+        legal = _flatten(self.board.get_all_possible_moves(self.color))
+        if not legal:
+            return Move([])
+        
+        # Evaluate each move and pick best
+        best_move = legal[0]
+        best_score = -999999
+        
+        for m in legal:
+            # Try the move
+            test_board = copy.deepcopy(self.board)
+            test_board.make_move(m, self.color)
+            
+            # Evaluate position
+            score = self.evaluate(test_board)
+            
+            if score > best_score:
+                best_score = score
+                best_move = m
+        
+        # applies the best move and returns the best move
+        self.board.make_move(best_move, self.color)
+        return best_move
+    
+    def evaluate(self, board):
+
+        if self.color == 1:  # Black
+            my_pieces = board.black_count
+            opp_pieces = board.white_count
+        else:  # White
+            my_pieces = board.white_count
+            opp_pieces = board.black_count
+        
+        return my_pieces - opp_pieces
